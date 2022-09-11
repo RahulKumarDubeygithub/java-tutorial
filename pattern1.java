b1ec0bc459d03c67f89f610098eedd8c31ce45c5
rahulkumardubey <rahul111brh@gmail.com>
2022-09-11 23:53:47 +0530

    pattern

---
+++ pattern1.java
@@ -0,0 +1,13 @@
+class pattern1{
+    public static void main(String args[]){
+        //outer loop
+        for(int i=0;i<4;i++){
+            //inner loop
+            for( int j=0;j<5;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
